c0b62b95e4044be5d8efe961ff47e9e1fd8aacea
Elkopasto <Elkopasto@gmail.com>
2023-02-10 01:55:26 +0400

    Icon

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
     <div className='app bg-beige'>
       <Head>
         <title>Elkopasto</title>
-        <meta name="description" content="Elkopasto" />
+        <meta name="description" content="Elkopasto's portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" href="/favicon.ico" />
 
